7f7c082ff5d9cf3e490ad96347bd9f1227b57fa7
LipeOzyy <fhenriquenatal@gmail.com>
2024-10-10 18:02:28 -0300

    Header

---
+++ Dns_mouse.py
@@ -0,0 +1,20 @@
+import requests
+import argparse
+
+GREEN = '\033[92m'
+RESET = '\033[0m'
+
+def show_header():
+    print(f"{GREEN}")
+    print("#########################################")
+    print("#         Subdir Scanner Tool           #")
+    print("#        Criado por: LipeOzzy           #")
+    print("#########################################")
+    print(f"{RESET}")
+
+# funcão principal para scan de diretorios
+
+
+    
+    
+    
