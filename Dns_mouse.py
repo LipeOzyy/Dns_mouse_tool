219e9aad8a47e2a1cdea3d0df023d599b97822c8
LipeOzyy <fhenriquenatal@gmail.com>
2024-10-10 18:12:06 -0300

    Função principal + algumas coisas

--- Dns_mouse.py
+++ Dns_mouse.py
@@ -12,9 +12,19 @@ def show_header():
     print("#########################################")
     print(f"{RESET}")
 
-# funcão principal para scan de diretorios
-
-
+# função para escanear os diretórios
+def scan_de_diretorios(url, wordlist):
+    print(f"[!] inciando a varredura em: {url}\n")
     
+    #le a wordlist
+    with open(wordlist, 'r') as f:
+        diretorios =f.read().splitlines()
     
-    
+    #adiciona sobre a url os subdiretorios da wordlist
+    for diretorio in diretorios:
+        diretorio_url = f"{url}/{diretorio}"
+        
+        try:
+            # envia uma requisição GET para o diretorio
+            responde = requests.get(diretorio_url)
+            
